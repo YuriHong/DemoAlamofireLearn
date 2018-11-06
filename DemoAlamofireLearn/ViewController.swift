2b68d971b62d8b891bac420e9d3c0e1059aedaeb
Yu Xue Qian <570046449@qq.com>
2018-11-06 10:05:54 +0800

    Initial Commit

---
+++ DemoAlamofireLearn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoAlamofireLearn
+//
+//  Created by TRS on 2018/11/6.
+//  Copyright © 2018年 TRS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
